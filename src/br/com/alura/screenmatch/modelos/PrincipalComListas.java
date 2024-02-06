2d5241ff1a2b364fe1a1f83109dc1e4a61cd6d2a
Augusto <augusto@inovacommerce.com.br>
2024-02-06 14:02:40 -0300

    aula 3

--- src/br/com/alura/screenmatch/modelos/PrincipalComListas.java
+++ src/br/com/alura/screenmatch/modelos/PrincipalComListas.java
@@ -4,20 +4,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PrincipalComListas {
-    Filme meuFilme = new Filme("O poderoso chefão", 1970);
-    Filme outroFilme = new Filme("Avatar", 2023);
-    Filme filmeDoPaulo = new Filme("Bastardos Inglorios", 2010);
-    Serie lost = new Serie("Lost", 2000);
     
-    //List<Titulo> lista = new ArrayList<>();
-    List<String> lista = new ArrayList<String>();
-    
-    //lista.add(filmeDoPaulo);
-    //lista.add(outroFilme);
-    //lista.add(outroFilme);
-    //lista.add(lost);
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme("O poderoso chefão", 1970);
+        meuFilme.avalia(10);
+        Filme outroFilme = new Filme("Avatar", 2023);
+        outroFilme.avalia(7);
+        Filme filmeDoPaulo = new Filme("Bastardos Inglorios", 2010);
+        filmeDoPaulo.avalia(8);
+        Serie lost = new Serie("Lost", 2000);
 
-    for(Titulo item:lista) {
-        System.out.println(item);
+        List<Titulo> lista = new ArrayList<>();
+        
+         lista.add(filmeDoPaulo);
+         lista.add(outroFilme);
+         lista.add(outroFilme);
+         lista.add(lost);
+
+        for (Titulo item: lista) {
+            System.out.println(item);
+            if(item instanceof Filme filme && filme.getClassificacao() > 2) {
+                System.out.println("Classficação: "+filme.getClassificacao());
+            }
+        }
     }
 }
